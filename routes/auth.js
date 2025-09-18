48db61331e5b3ce4722a5709c1cacb0cc8cba75e
Hoang <Hoangminhquy032003@gmail.com>
2025-09-18 17:30:02 +0700

    Hoàn chỉnh route đăng ký và đăng nhập

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User"); // ✅ Đường dẫn tương đối, không dùng đường dẫn tuyệt đối
+const User = require("../models/user"); // ✅ Đường dẫn tương đối, không dùng đường dẫn tuyệt đối
 
 const router = express.Router();
 
